bc21b2701790239905568570129e6b1024c32cda
agustin.m <agusfede724@gmail.com>
2025-11-26 23:00:42 -0300

    first commit

--- api/index.js
+++ api/index.js
@@ -1,15 +1,20 @@
-// api/index.js
 import express from "express";
+import cors from "cors";
 import authRoutes from "../routes/authRoutes.js";
 import eventRoutes from "../routes/eventRoutes.js";
-import cors from "cors";
 
 const app = express();
 
-app.use(cors());
 app.use(express.json());
 
+// ✅ Configura CORS para tu frontend en producción
+app.use(cors({
+    origin: "https://frontend-utn-eventos.vercel.app",
+    credentials: true // si estás usando cookies / headers de autenticación
+}));
+
+// Rutas
 app.use("/api/auth", authRoutes);
 app.use("/api/events", eventRoutes);
 
-export default app; // <- Muy importante
+export default app;
