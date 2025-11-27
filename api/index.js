996227f4ce1b7378fe229862682d06501ee4bfff
agustin.m <agusfede724@gmail.com>
2025-11-26 22:56:51 -0300

    first commit

--- api/index.js
+++ api/index.js
@@ -1,32 +1,15 @@
+// api/index.js
 import express from "express";
-import dotenv from "dotenv";
+import authRoutes from "../routes/authRoutes.js";
+import eventRoutes from "../routes/eventRoutes.js";
 import cors from "cors";
-import conectarDB from "../src/config/db.js";
-import authRoutes from "../src/routes/authRoutes.js";
-import eventRoutes from "../src/routes/eventRoutes.js";
 
-dotenv.config();
 const app = express();
 
-// Conectar a MongoDB
-conectarDB();
-
-// CORS
-app.use(cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true,
-}));
-
-// Parseo JSON
+app.use(cors());
 app.use(express.json());
 
-// Rutas
 app.use("/api/auth", authRoutes);
 app.use("/api/events", eventRoutes);
 
-// Ruta ping para despertar el backend
-app.get("/api/auth/ping", (req, res) => {
-    res.json({ ok: true });
-});
-
-export default app;
+export default app; // <- Muy importante
